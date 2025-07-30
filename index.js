e47b4d01aee9016231793cd8631ef5f74638c561
Aditya-giri1 <adityagiri8520@gmail.com>
2025-07-30 21:07:44 +0530

    Added auth middelware

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const JWT_SECRET = "ADITYAGIRI10";
 const app = express();
 app.use(express.json());
 const user = [];
+function auth(req, res, next){
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, JWT_SECRET, function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                message: "Invalid token"
+            });
+        }
+        req.username = decoded.username;
+        // If token is valid, proceed to the next middleware or route handlerf
+        next();
+    });
+}
 
 app.post("/signup", function(req,res){
     const username = req.body.username;
@@ -39,26 +52,13 @@ app.post("/signin", function(req,res){
     }
     
 })
-app.get("/protected", function(req, res){
-    const token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({
-            message: "Access denied, no token provided"
-        })
-
-}
-    jwt.verify(token, JWT_SECRET, function(err, decoded) {
-        if (err) {
-            return res.status(401).json({
-                message: "Invalid token"
-            });
-        }
+app.get("/protected", auth,function(req, res){
+     
         res.json({
             message: "Protected data accessed",
-            user: decoded.username
+            user: req.username
         });
     });
-})
  
 app.listen(3000, function(){
     console.log("Server started on port 3000");
